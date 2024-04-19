2a4cca0aeeb307082ef109fe8b294ea870839189
Adriigbs <adrian.graur@tecnico.ulisboa.pt>
2024-04-19 18:06:53 +0100

    Parse instance and adjency functions

--- pipe.py
+++ pipe.py
@@ -1,10 +1,15 @@
-class PipeManiaState:
+from sys import stdin
+
+
+
+
+class State:
     state_id = 0
 
     def __init__(self, board):
         self.board = board
-        self.id = PipeManiaState.state_id
-        PipeManiaState.state_id += 1
+        self.id = State.state_id
+        State.state_id += 1
 
 
     def __lt__(self, other):
@@ -16,37 +21,69 @@ class PipeManiaState:
 class Board:
     """ Representação interna de uma grelha de PipeMania. """
 
+    grid = [] # Lista de listas de strings
+
     def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
         """ Devolve os valores imediatamente acima e abaixo,
         respectivamente. """
-        # TODO
-        pass
+        result = ()
+        
+        
+        if(len(self.grid) - 1 == row):          #last row case
+            result += (self.grid[row-1][col],)
+            result += (None,)
+
+        elif(row == 0):                         #first row case
+            result += (None,)
+            result += (self.grid[row+1][col],)
+
+        else:
+            result += (self.grid[row-1][col],)
+            result += (self.grid[row+1][col],)
+
+        return result
+        
 
 
     def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
         """ Devolve os valores imediatamente à esquerda e à direita,
         respectivamente. """
-        # TODO
-        pass
+        result = ()
+        
+        if(col == 0):
+            result += (None,)
+            result += (self.grid[row][col+1],)
+        
+        elif(len(self.grid[row]) - 1 == col):     
+            result += (self.grid[row][col-1],)
+            result += (None,)
+
+        else:
+            result += (self.grid[row][col-1],)
+            result += (self.grid[row][col+1],)
+
+        return result
+        
 
     # TODO: outros metodos da classe
 
     @staticmethod
     def parse_instance():
         """Lê a instância do problema do standard input (stdin)
-        e retorna uma instância da classe Board.
+        e retorna uma instância da classe Board."""
 
-        Por exemplo:
-            $ python3 pipe_mania.py < input_T01
-            > from sys import stdin
-            > line = stdin.readline().split()
-        """
-        # TODO
-        pass
+        board = Board()
+
+        lines = stdin.readlines()
+        for line in lines:
+            line = line.strip()
+            board.grid.append(line.split())
+
+        return board
 
 
 
-
+'''
 class PipeMania(Problem):
     def __init__(self, initial_state: Board, goal_state: Board):
         """ O construtor especifica o estado inicial. """
@@ -74,3 +111,14 @@ class PipeMania(Problem):
         """ Função heuristica utilizada para a procura A*. """
         # TODO
         pass
+'''
+
+
+
+# Teste da classe Board
+board = Board.parse_instance()
+
+print(board.adjacent_vertical_values(0, 0))
+print(board.adjacent_horizontal_values(0, 0))
+print(board.adjacent_vertical_values(1, 1))
+print(board.adjacent_horizontal_values(1, 1))
